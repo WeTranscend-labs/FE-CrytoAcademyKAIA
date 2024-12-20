a80244f28b13954a3249304ddbd148213623d34a
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-12-20 14:41:03 +0700

    feat: add Background and Testimonials components to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import CourseList from "@/components/courses/course-list";
+import Background from "@/components/eldoraui/novatrixbg";
 import Features from "@/components/home/features";
 import Hero from "@/components/home/hero";
+import Testimonials from "@/components/home/TestimonalSliderDemo";
+
 
 
 
 export default function Home() {
   return (
     <div className="space-y-16">
+
       <Hero />
+      <Testimonials />
       <Features />
       <CourseList />
     </div>
